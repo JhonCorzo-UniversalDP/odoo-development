adc33d29808278181f723ccf8bb36cad745b0686
Jhon Corzo <jcorzo@universaldeproductossa.com>
2025-07-09 13:29:10 +0000

    [2.02] FIX: fields misspelled

--- motorcycle_financing/models/loan_application.py
+++ motorcycle_financing/models/loan_application.py
@@ -6,18 +6,18 @@ class LoanApplication(models.Model):
 
     
     name = fields.Char(string='Application Number', required=True)
-    currency_id = field.Many2one(string='Currency',comodel_name='res.currency')
-    date_application = field.Date(string='Application Date')
-    date_approval = field.Date(string='Approval Date')
-    date_rejection = field.Date(string='Rejection Date')
-    date_signed = field.Date(string='Signed On')
-    down_payment = field.Monetary(string='Downpayment', currency_field='currency_id')
-    interest_rate = field.Float(string='Interest Rate (%)', digits=(5, 4))
-    loan_amount = field.Monetary(string='Loan Amount', currency_field='currency_id')
-    loan_term = field.Integer(string='Loan Term (Months)', required=True, default=36)
-    rejection_reason = field.Text(string='Rejection Reason')
-    state = field.Selection(string='Status', selection=[('draft', 'Draft'), ('sent', 'Sent'), ('review', 'Credit Check'), ('approved', 'Approved'), ('rejected', 'Rejected'), ('signed', 'Signed'), ('cancel', 'Canceled')], default='draft')
-    notes = field.Html(string='Notes')
+    currency_id = fields..Many2one(string='Currency',comodel_name='res.currency')
+    date_application = fields..Date(string='Application Date')
+    date_approval = fields..Date(string='Approval Date')
+    date_rejection = fields..Date(string='Rejection Date')
+    date_signed = fields..Date(string='Signed On')
+    down_payment = fields..Monetary(string='Downpayment', currency_field='currency_id')
+    interest_rate = fields..Float(string='Interest Rate (%)', digits=(5, 4))
+    loan_amount = fields..Monetary(string='Loan Amount', currency_field='currency_id')
+    loan_term = fields..Integer(string='Loan Term (Months)', required=True, default=36)
+    rejection_reason = fields..Text(string='Rejection Reason')
+    state = fields..Selection(string='Status', selection=[('draft', 'Draft'), ('sent', 'Sent'), ('review', 'Credit Check'), ('approved', 'Approved'), ('rejected', 'Rejected'), ('signed', 'Signed'), ('cancel', 'Canceled')], default='draft')
+    notes = fields..Html(string='Notes')
     
     
     
