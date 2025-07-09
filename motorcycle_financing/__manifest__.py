0b0762eaf90dd35b3c24ddd62af2a8c0d83232bc
Jhon Corzo <jcorzo@universaldeproductossa.com>
2025-07-09 02:48:11 +0000

    [2.01] FIX: manifest sintax errors

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -1,10 +1,10 @@
 {
     'name': "Motorcycle Financing",
-    'summary' : "Streamlines the loan application process for dealerships.",
+    'summary': "Streamlines the loan application process for dealerships.",
     'category': 'Kawiil/Custom Modules',
-    'version' : '18.0.0.0.1',
-    'website' : "https://github.com/JhonCorzo-UniversalDP/odoo-development",
-    'author' : "Jhon Jairo Corzo Calderon",
-    'license' : "OPL-1",
-    'application' : True,
+    'version': '18.0.0.0.1',
+    'website': 'https://github.com/JhonCorzo-UniversalDP/odoo-development',
+    'author': "Jhon Jairo Corzo Calderon",
+    'license': 'OPL-1',
+    'application': True,
 }
