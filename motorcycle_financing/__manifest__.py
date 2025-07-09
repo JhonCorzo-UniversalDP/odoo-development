6d58a7716fc1bba1ca1415d8b0f2bcd19d46a33b
Jhon Corzo <jcorzo@universaldeproductossa.com>
2025-07-09 02:55:43 +0000

    [2.01] ADD: manifest updated

--- motorcycle_financing/__manifest__.py
+++ motorcycle_financing/__manifest__.py
@@ -1,10 +1,14 @@
 {
     'name': "Motorcycle Financing",
     'summary': "Streamlines the loan application process for dealerships.",
+    'description': 'Modulo encargado de llevar control sobre los prestamos realizado en Kawiil Motors',
     'category': 'Kawiil/Custom Modules',
     'version': '18.0.0.0.1',
     'website': 'https://github.com/JhonCorzo-UniversalDP/odoo-development',
     'author': "Jhon Jairo Corzo Calderon",
     'license': 'OPL-1',
+    'depends': [],
+	​'data': [],
+	​'demo': [],
     'application': True,
 }
